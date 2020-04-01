37d00ab24ee9e2adf7a7705b802c8df741c85a9b
Ángel Cabanilles <angel.cabanilles@the-cocktail.com>
2020-04-01 15:36:48 +0200

    Improved recipe

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ var pizzaTotalSet = false;
 
 $(document).ready(function(){
 
-
-
     $('#flour').change(function() { 
         var flour = parseFloat($('#flour').val());
         calculateValuesBasedOnFlour(flour)
@@ -13,6 +11,12 @@ $(document).ready(function(){
     $('#water').change(function() { 
         var water = parseFloat($('#water').val());
         var flour = water / 0.65;
+        calculateValuesBasedOnFlour(flour)
+    });
+
+    $('#sugar').change(function() { 
+        var water = parseFloat($('#sugar').val());
+        var flour = sugar / 0.015;
         calculateValuesBasedOnFlour(flour)
     });
 
@@ -36,38 +40,36 @@ $(document).ready(function(){
 
     $('#pizzaTotal').change(function() { 
         var pizzaTotal = parseFloat($('#pizzaTotal').val());
-        var flour = pizzaTotal * 146;
+        var flour = pizzaTotal * 190;
         var pizzaTotalSet = true;
         calculateValuesBasedOnFlour(flour);
     });
 
-
-
 });
 
 function calculateValuesBasedOnFlour(flour){
-        var water = flour * 0.65;
+        var water = flour * 0.63;
         var salt = flour * 0.03;
         var oil = flour * 0.03;
         var yeast = flour * 0.002;
+        var sugar = flour * 0.015;
 
-        setValues(flour, water, salt, yeast, oil)
+        setValues(flour, water, salt, yeast, oil, sugar)
 }
 
-function setValues(flour, water, salt, yeast, oil, pizzaTotal){
+function setValues(flour, water, salt, yeast, oil, sugar, pizzaTotal){
     $('#flour').val(Math.round( Math.abs(flour * 10 )) / 10);
     $('#water').val(Math.round( Math.abs(water * 10 )) / 10);
     $('#salt').val(Math.round( Math.abs(salt * 10 )) / 10);
     $('#yeast').val(Math.round( Math.abs(yeast * 10 )) / 10);
     $('#oil').val(Math.round( Math.abs(oil * 10 )) / 10);
-
-    console.log(pizzaTotalSet)
+    $('#sugar').val(Math.round( Math.abs(sugar * 10 )) / 10);
 
     if(pizzaTotalSet == true) {
         pizzaTotalSet = false;
     }
     else {
-        var pizzaTotal = (water + salt + flour + yeast + oil) / 250;
+        var pizzaTotal = (water + salt + flour + yeast + oil) / 320;
         $('#pizzaTotal').val(Math.round( Math.abs(pizzaTotal * 10 )) / 10);
         pizzaTotalSet = false;
     }
